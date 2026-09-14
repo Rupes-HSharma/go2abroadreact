ccb27b49d494953ff9f10de89bcc9e19c7373ebe
Rupes-HSharma <rupeshanexpert@gmail.com>
2026-09-14 21:26:57 +0530

    Fix GitHub Pages routing and asset paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
@@ -13,21 +13,26 @@ import ArizonaStateUniversity from "./pages/ArizonaStateUniversity";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/destinations" element={<Destinations />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/success-stories" element={<SuccessStories />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/usa" element={<Usa />} />
-          <Route path="/arizona-state-university" element={<ArizonaStateUniversity />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    
+     <Routes>
+
+  <Route element={<Layout />}>
+    <Route path="/" element={<Home />} />
+    <Route path="/about-us" element={<AboutUs />} />
+    <Route path="/services" element={<Services />} />
+    <Route path="/destinations" element={<Destinations />} />
+    <Route path="/courses" element={<Courses />} />
+    <Route path="/success-stories" element={<SuccessStories />} />
+    <Route path="/faq" element={<Faq />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/usa" element={<Usa />} />
+    <Route
+      path="/arizona-state-university"
+      element={<ArizonaStateUniversity />}
+    />
+  </Route>
+
+</Routes>
+ 
   );
 }
